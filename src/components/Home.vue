ffe66089042458bc1ecda6b4c857de8d9220a116
Phil Pan <210254332@qq.com>
2020-09-18 23:07:11 +0800

    完成登录退出功能

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-button type="info" @click="logOut">退出</el-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    logOut: function() {
+      setTimeout(() => {
+        window.sessionStorage.clear();
+        this.$router.push("/login");
+      }, 1000);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,45 @@
 import Vue from "vue";
-import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import App from "./App.vue";
+import "./plugins/element.js";
+import "./assets/css/reset.css";
+import "./assets/css/global.css";
+import "element-ui/lib/theme-chalk/index.css";
+import axios from "axios";
+/* axios.create({
+  baseURL: "http://timemeetyou.com:8889/api/private/v1/",
+  // 跨域请求时是否需要使用凭证
+  withCredentials: true,
+  timeout: 10000,
+  headers: {
+    get: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+      // 在开发中，一般还需要单点登录或者其他功能的通用请求头，可以一并配置进来
+    },
+    post: {
+      "Content-Type": "application/json;charset=utf-8"
+      // 在开发中，一般还需要单点登录或者其他功能的通用请求头，可以一并配置进来
+    }
+  }
+}); */
+axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/";
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+
+// 自定义格式化时间的全局过滤器
+Vue.filter("dateFormat", function(originVal) {
+  const dt = new Date(originVal);
+
+  const y = dt.getFullYear();
+  const m = (dt.getMonth() + 1 + "").padStart(2, "0");
+  const d = (dt.getDate() + "").padStart(2, "0");
+  //padStart(2,'0') 意思是不足两位自动补0
+  const hh = (dt.getHours() + "").padStart(2, "0");
+  const mm = (dt.getMinutes() + "").padStart(2, "0");
+  const ss = (dt.getSeconds() + "").padStart(2, "0");
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`;
+});
 
 new Vue({
   router,
